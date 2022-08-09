efda9f8cb5564a34863827941d86d37b54e6c5de
D3stros <patrick.fallmann@hotmail.com>
2022-08-09 23:38:37 +0200

    Creating message controller

---
+++ src/main/java/com/example/demo/MessageRequest.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class MessageRequest {
+    private final String message;
+
+    public MessageRequest(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
